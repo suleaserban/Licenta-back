2a074a55d7e52ee02ce62f70209c4d88642263ce
Sulea Serban <serban.sulea@ibm.com>
2023-10-28 18:33:03 +0300

    configs

---
+++ src/main/java/com/example/nutriCare/NutriCareApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutriCare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nutriCare/NutriCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutriCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
